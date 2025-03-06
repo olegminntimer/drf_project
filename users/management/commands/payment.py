fdd1745e91ff649a94c316d1b826eb389a62eead
Oleg Timershayakhov <olegtimer@yandex.ru>
2025-03-06 10:09:27 +0500

    add command add_user, payment

--- users/management/commands/payment.py
+++ users/management/commands/payment.py
@@ -13,10 +13,40 @@ class Command(BaseCommand):
             )
             course.save()
 
+        if not Course.objects.filter(name='Python dev').exists():
+            course = Course(
+                name="Python dev",
+                description="Python development",
+            )
+            course.save()
+
+        if not Course.objects.filter(name='Java dev').exists():
+            course = Course(
+                name="Java dev",
+                description="Java development",
+            )
+            course.save()
+
         payment = Payment(
             user=User.objects.get(email="user@sky.com"),
             course_paid=Course.objects.get(name="Python"),
             amount=100000,
             payment_method="cash",
+        )
+        payment.save()
+
+        payment = Payment(
+            user=User.objects.get(email="user2@sky.pro"),
+            course_paid=Course.objects.get(name="Python dev"),
+            amount=110000,
+            payment_method="bank_transfer",
+        )
+        payment.save()
+
+        payment = Payment(
+            user=User.objects.get(email="user@sky.com"),
+            course_paid=Course.objects.get(name="Java dev"),
+            amount=120000,
+            payment_method="bank_transfer",
         )
         payment.save()
