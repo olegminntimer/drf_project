b305db217c1bbdc4d6cd1067d5fcf998a4940a30
olegminntimer <olegtimer@yandex.ru>
2025-03-11 11:42:15 +0500

    Revert "Develop"

--- lms/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from lms.models import Lesson
-
-
-@admin.register(Lesson)
-class LessonAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'preview', 'video', 'course')
