87f6bf776654384ee3b1d5e07431c8aa07715af0
Oleg Timershayakhov <olegtimer@yandex.ru>
2025-03-12 11:01:12 +0500

    add usercreate

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,13 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from users.apps import UsersConfig
-from users.views import PaymentViewSet
+from users.views import PaymentViewSet, UserCreateAPIView
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = UsersConfig.name
 
@@ -9,7 +15,9 @@ router = DefaultRouter()
 router.register("payment", PaymentViewSet)
 
 urlpatterns = [
-
+    path('register/', UserCreateAPIView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns += router.urls
--- users/views.py
+++ users/views.py
@@ -1,14 +1,24 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import CreateAPIView
 
-from users.models import Payment
-from users.serializers import PaymentSerializer
+from users.models import Payment, User
+from users.serializers import PaymentSerializer, UserSerializer
 
 
 class PaymentViewSet(ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
-    filter_backends = [filters.OrderingFilter]
+    filter_backends = [filters.OrderingFilter, DjangoFilterBackend]
     filterset_fields = ['course_paid', 'lesson_paid', 'payment_method']
-    ordering_fields = ('data',)
+    ordering_fields = ['data',]
+
+class UserCreateAPIView(CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
