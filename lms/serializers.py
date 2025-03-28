c82fff1fe6738730b3b0f88a282015cc6e200d75
Oleg Timershayakhov <olegtimer@yandex.ru>
2025-03-28 18:14:33 +0500

    add Indication of current user's subscription to the course

--- lms/serializers.py
+++ lms/serializers.py
@@ -2,17 +2,24 @@ from rest_framework import serializers
 
 from lms.models import Course, Lesson
 from lms.validators import validate_allowed_sites
+from users.models import Subscription
 
 
 class CourseSerializer(serializers.ModelSerializer):
     lessons = serializers.SerializerMethodField()
     count_lesson = serializers.SerializerMethodField()
-
+    user_subs = serializers.SerializerMethodField()
     def get_lessons(self, course):
         return [lesson.name for lesson in Lesson.objects.filter(course=course)]
 
     def get_count_lesson(self, course):
         return Lesson.objects.filter(course=course).count()
+
+    def get_user_subs(self, course):
+        # user = validated_data.context['context'].user.pk
+        # course = validated_data['course']
+        user = self.context['request'].user
+        return Subscription.objects.filter(user=user, course=course).exists()
 
     class Meta:
         model = Course
