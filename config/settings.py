d712dba526f1ec4128eac32486a08076b92830a1
Oleg Timershayakhov <olegtimer@yandex.ru>
2025-04-26 16:02:13 +0500

    deploy

--- config/settings.py
+++ config/settings.py
@@ -114,6 +114,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
@@ -132,7 +134,7 @@ STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://redis:6379/1",
     }
 }
 
