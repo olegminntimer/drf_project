9c9847bdbf3094b6eedae9bb3899a8c3a5599259
Oleg Timershayakhov <olegtimer@yandex.ru>
2025-05-04 13:52:53 +0500

    env4 test2 -

--- lms/tests.py
+++ lms/tests.py
@@ -1,9 +1,9 @@
-from django.urls import reverse
-from rest_framework import status
-from rest_framework.test import APITestCase
-
-from lms.models import Course, Lesson
-from users.models import User
+# from django.urls import reverse
+# from rest_framework import status
+# from rest_framework.test import APITestCase
+#
+# from lms.models import Course, Lesson
+# from users.models import User
 
 
 # class LessonAPITestCase(APITestCase):
