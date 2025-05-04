54caa10cb2355fcc25b85e41066668b22a1a5c62
Oleg Timershayakhov <olegtimer@yandex.ru>
2025-05-04 11:20:15 +0500

    correct

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 import os
-import certifi
 import sys
+import certifi
 from datetime import timedelta
 from pathlib import Path
 
@@ -14,12 +14,17 @@ SERVER_TYPE = os.environ.get("SERVER_TYPE", "")
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECRET_KEY = os.getenv("SECRET_KEY")
-SECRET_KEY = '^phwIS,8986MVs7XkcHjK8se{1#~>]FDoimNN2j-N^_t:F8F'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = True if os.getenv("DEBUG") == "True" else False
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["*"]
+if not SECRET_KEY and not DEBUG:
+    raise ValueError("SECRET_KEY must be set in production")
+
+# ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",") if os.getenv("ALLOWED_HOSTS") else []
+if DEBUG:
+    ALLOWED_HOSTS.extend(["localhost", "127.0.0.1", "0.0.0.0"])
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -85,19 +90,20 @@ if 'test' in sys.argv or os.getenv('GITHUB_ACTIONS') == 'true':
         }
     }
 else:
-    if SERVER_TYPE == "local":
-        host_db = "127.0.0.1"
-    else:
-        host_db = "db"
+    # if SERVER_TYPE == "local":
+    #     host_db = "127.0.0.1"
+    # else:
+    #     host_db = "db"
 
+    # Установлены fallback-значения для переменных окружения, чтобы не вызвать ошибки, если они не заданы.
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": os.getenv("POSTGRES_DB"),
-            "USER": os.getenv("POSTGRES_USER"),
-            "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-            "HOST": host_db,
-            "PORT": os.getenv("POSTGRES_PORT"),
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("POSTGRES_DB", "postgres"),
+            "USER": os.getenv("POSTGRES_USER", "postgres"),
+            "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
+            "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+            "PORT": os.getenv("POSTGRES_PORT", "5432"),
         }
     }
 
@@ -126,12 +132,11 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = '/app/staticfiles/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_URL = "media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
@@ -144,20 +149,32 @@ SIMPLE_JWT = {
 
 STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
 
+
+REDIS_HOST = "redis" if os.getenv("DOCKER_ENV") == "true" else "127.0.0.1"
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://redis:6379/1",
+        "LOCATION": f"redis://{REDIS_HOST}:6379/1",
     }
 }
 
-# Настройки для Celery
+CELERY_BROKER_URL = f"redis://{REDIS_HOST}:6379/0"
+CELERY_RESULT_BACKEND = f"redis://{REDIS_HOST}:6379/0"
 
-# URL-адрес брокера сообщений
-CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"  # Например, Redis, который по умолчанию работает на порту 6379
-
-# URL-адрес брокера результатов, также Redis
-CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django.core.cache.backends.redis.RedisCache",
+#         "LOCATION": "redis://redis:6379/1",
+#     }
+# }
+#
+# # Настройки для Celery
+#
+# # URL-адрес брокера сообщений
+# CELERY_BROKER_URL = "redis://redis:6379/0"  # Например, Redis, который по умолчанию работает на порту 6379
+#
+# # URL-адрес брокера результатов, также Redis
+# CELERY_RESULT_BACKEND = "redis://redis:6379/0"
 
 # Часовой пояс для работы Celery
 CELERY_TIMEZONE = TIME_ZONE
@@ -174,7 +191,7 @@ CELERY_BEAT_SCHEDULE = {
     "task-check_last_login": {
         "task": "lms.tasks.check_last_login",  # Путь к задаче
         "schedule": timedelta(
-            seconds=10
+            hours=1
         ),  # Расписание выполнения задачи (например, каждые 10 минут)
     },
 }
@@ -185,8 +202,19 @@ EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = True if os.getenv("EMAIL_USE_TLS") == "True" else False
-EMAIL_USE_SSL = True if os.getenv("EMAIL_USE_SSL") == "True" else False
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "False") == "True"
+EMAIL_USE_SSL = not EMAIL_USE_TLS and (os.getenv("EMAIL_USE_SSL", "False") == "True")
+if EMAIL_USE_TLS and EMAIL_USE_SSL:
+    raise ValueError("EMAIL_USE_TLS and EMAIL_USE_SSL cannot be both True")
 
 SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+# Настройки для production
+if not DEBUG:
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    CSRF_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_HTTPONLY = True
