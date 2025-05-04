9cc4ed14d2d7a2f707bbdd96b17eda402c0d8344
Oleg Timershayakhov <olegtimer@yandex.ru>
2025-05-04 14:32:47 +0500

    test 2

--- users/tests.py
+++ users/tests.py
@@ -1,9 +1,11 @@
+import json
+
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
 from lms.models import Course
-from users.models import User
+from users.models import User, Subscription
 
 
 class SubscriptionAPITestCase(APITestCase):
@@ -22,27 +24,35 @@ class SubscriptionAPITestCase(APITestCase):
         }
         response = self.client.post(
             url,
-            data,
+            data=json.dumps(data), # Явно сериализуем в JSON
             content_type='application/json'
         )
-        result = response.json()
-        self.assertEqual(
-            response.status_code, status.HTTP_200_OK
-        )
-        self.assertEqual(
-            result, {'message': 'Вы подписались на обновления курса'}
-        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(Subscription.objects.filter(user=self.user, course=self.course).exists())
+
+        # result = response.json()
+        # self.assertEqual(
+        #     response.status_code, status.HTTP_200_OK
+        # )
+        # self.assertEqual(
+        #     result, {'message': 'Вы подписались на обновления курса'}
+        # )
 
         url = reverse("users:subscription")
         data = {
             "user": self.user.pk,
             "course": self.course.pk
         }
-        response = self.client.post(url, data)
-        result = response.json()
+        response = self.client.post(
+            url,
+            data=json.dumps(data),
+            content_type='application/json'
+        )
+        # result = response.json()
         self.assertEqual(
             response.status_code, status.HTTP_200_OK
         )
-        self.assertEqual(
-            result, {'message': 'Вы отписались от обновления курса'}
-        )
+        # self.assertEqual(
+        #     result, {'message': 'Вы отписались от обновления курса'}
+        # )
+        self.assertFalse(Subscription.objects.filter(user=self.user, course=self.course).exists())
